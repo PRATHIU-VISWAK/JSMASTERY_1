8cdf1c21762c1c91a4b6b7fe10cd4ce1a713ee4e
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-03-17 07:06:41 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import MovieCard from "../src/components/MovieCard";
+import axios from "axios";
+
 //5a20e59a
 function App() {
-  const [count, setCount] = useState(0)
-
-  const API_URL = 'https://www.omdbapi.com/?apikey=5a20e59a'
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [name, setName] = useState("superman");
+  const API_URL = "https://www.omdbapi.com/?apikey=5a20e59a";
 
   const fetching = async (title) => {
-    try{
-    const response = await axios.get(`${API_URL}&s=${title}`)
-    console.log(response);
-
-    }catch(error){
-      console.log("axios error",error);
+    try {
+      const response = await axios.get(`${API_URL}&s=${title}`);
+      setMovies(response.data.Search);
+      console.log(response.data.Search);
+    } catch (error) {
+      console.log("axios error", error);
+    } finally {
+      setisLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetching("superman")
-  },[])
-
+    fetching("superman");
+  }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Movie Land</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+
+        <button
+          onClick={() => {
+            fetching(name);
+          }}
+        >
+          {" "}
+          click
+        </button>
+      </div>
+
+      <div className="container">
+        
+
+        {isLoading ? <h1>Loading </h1> : 
+        <>
+        {
+           movies.map((movie, index) => (
+            <MovieCard key={index}  movie={movie}/>
+           ))
+        }
+
+        </>
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
